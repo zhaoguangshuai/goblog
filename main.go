a85173f9e8ba28ffa62d9b5a7596a11ddc6c39c8
赵光帅 <969426998zgs@gmail.com>
2021-02-01 21:09:04 +0800

    404 状态码

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	} else if r.URL.Path == "/about" {
 		fmt.Fprint(w,"此博客是用以记录编程笔记，如您有反馈或建议，请联系"+"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 	} else {
+		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
 			"<p>如有疑惑，请联系我们。</p>")
 	}
