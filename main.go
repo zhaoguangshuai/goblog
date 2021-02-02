98c333efc711dc635416aba3dd4c5cb26d24bb19
赵光帅 <969426998zgs@gmail.com>
2021-02-02 15:23:44 +0800

    读取请求数据

--- main.go
+++ main.go
@@ -8,9 +8,10 @@ import (
 )
 
 var router = mux.NewRouter()
-func homeHandler(w http.ResponseWriter, r *http.Request)  {
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	//w.Header().Set("Content-Type","text/html;charset=utf-8")
-	fmt.Fprint(w,"<h1>hello，欢迎来到goblog！</h1>")
+	fmt.Fprint(w, "<h1>hello，欢迎来到goblog！</h1>")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,35 +20,53 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 }
 
-func notFoundHandler(w http.ResponseWriter,r *http.Request)  {
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	//设置返回行的内容类型是xml，还是json，还是文本内容；该设置为html类型，不然解析不了
 	//w.Header().Set("Content-Type","text/html;charset=utf-8")
 	//设置返回的http状态码为404
 	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w,"<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
+	fmt.Fprint(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
 }
 
-func articlesShowHandler(w http.ResponseWriter,r *http.Request)  {
+func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 	fmt.Fprint(w, "文章 ID:"+id)
 }
 
-func articlesIndexHandler(w http.ResponseWriter,r *http.Request)  {
-	fmt.Fprint(w,"访问文章列表11")
+func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "访问文章列表11")
 }
 
-func articlesStoreHandler(w http.ResponseWriter,r *http.Request)  {
-	fmt.Fprint(w,"创建新的文章")
+func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
+	//err := r.ParseForm()
+	//if err != nil {
+	//	//解析错误，这里 应该有错误处理
+	//	fmt.Fprint(w, "请提供正确的数据！")
+	//	return
+	//}
+	//title := r.PostForm.Get("title")
+	//
+	//fmt.Fprintf(w, "POST PostForm:%v<br>", r.PostForm)
+	//fmt.Fprintf(w, "POST Form:%v<br>", r.Form)
+	//fmt.Fprintf(w, "POST title:%v<br>", title)
+	fmt.Fprintf(w,"r.Form 中 title 的值为：%v<br/>>",r.FormValue("title"))
+	fmt.Fprintf(w,"r.PostForm 中 title 的值为：%v<br/>>",r.PostFormValue("title"))
+	fmt.Fprintf(w,"r.Form 中 test 的值为：%v<br/>>",r.FormValue("test"))
+	fmt.Fprintf(w,"r.PostForm 中 test 的值为：%v<br/>>",r.PostFormValue("test"))
+
+
+
+
 }
 
 //添加一个设置返回内容格式的中间件
 func forceHTMLMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		//1.设置标头
-		w.Header().Set("Content-Type","text/html;charset=utf-8")
+		w.Header().Set("Content-Type", "text/html;charset=utf-8")
 		//2.继续处理请求
-		next.ServeHTTP(w,r)
+		next.ServeHTTP(w, r)
 	})
 }
 
@@ -55,10 +74,10 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		//1.除首页以外，移除所有请求路径后面的斜杠
 		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path,"/")
+			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
 		}
 		//2.将请求继续传递下去
-		next.ServeHTTP(w,r)
+		next.ServeHTTP(w, r)
 	})
 }
 
@@ -70,7 +89,7 @@ func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
    <title>创建文章 —— 我的技术博客</title>
 </head>
 <body>
-   <form action="/articles" method="post">
+   <form action="/articles?test=data" method="post">
        <p><input type="text" name="title"></p>
        <p><textarea name="body" cols="30" rows="10"></textarea></p>
        <p><button type="submit">提交</button></p>
@@ -89,13 +108,13 @@ func main() {
 
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 
-	router.HandleFunc("/articles/{id:[0-9]+}",articlesShowHandler).Methods("GET").Name("articles.show")
+	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
 
-	router.HandleFunc("/articles",articlesIndexHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
 
-	router.HandleFunc("/articles",articlesStoreHandler).Methods("POST").Name("articles.shore")
+	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.shore")
 
-	router.HandleFunc("/articles/create",articlesCreateHandler).Methods("GET").Name("articles.create")
+	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
 
 	//自定义404页面
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
