7e7525c56df04609e989f7cba6b4d6938ce30767
赵光帅 <969426998zgs@gmail.com>
2021-02-02 09:33:17 +0800

    集成 Gorilla Mux 路由

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module goblog
 
 go 1.15
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html;charset=utf-8")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>hello,欢迎来到 goblog</h1>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
-			"<p>如有疑惑，请联系我们。</p>")
-	}
-
+func homeHandler(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type","text/html;charset=utf-8")
+	fmt.Fprint(w,"<h1>hello，欢迎来到goblog！</h1>")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
@@ -23,26 +17,50 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 }
 
+func notFoundHandler(w http.ResponseWriter,r *http.Request)  {
+	//设置返回行的内容类型是xml，还是json，还是文本内容；该设置为html类型，不然解析不了
+	w.Header().Set("Content-Type","text/html;charset=utf-8")
+	//设置返回的http状态码为404
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w,"<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
+}
+
+func articlesShowHandler(w http.ResponseWriter,r *http.Request)  {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Fprint(w, "文章 ID:"+id)
+}
+
+func articlesIndexHandler(w http.ResponseWriter,r *http.Request)  {
+	fmt.Fprint(w,"访问文章列表")
+}
+
+func articlesStoreHandler(w http.ResponseWriter,r *http.Request)  {
+	fmt.Fprint(w,"创建新的文章")
+}
+
 func main() {
-	//http.HandleFunc("/", defaultHandler)
-	//http.HandleFunc("/about", aboutHandler)
-	router := http.NewServeMux()
+	router := mux.NewRouter()
 
-	router.HandleFunc("/", defaultHandler)
-	router.HandleFunc("/about", aboutHandler)
+	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 
-	//文章详情
-	router.HandleFunc("/articles", func(writer http.ResponseWriter, request *http.Request) {
-		//id := strings.SplitN(request.URL.Path,"/",3)[2]
-		//fmt.Println(id)
+	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 
-		switch request.Method {
-		case "GET":
-			fmt.Fprint(writer,"访问文章列表")
-		case "POST":
-			fmt.Fprint(writer,"创建新的文章")
-		}
-	})
+	router.HandleFunc("/articles/{id:[0-9]+}",articlesShowHandler).Methods("GET").Name("articles.show")
+
+	router.HandleFunc("/articles",articlesIndexHandler).Methods("GET").Name("articles.index")
+
+	router.HandleFunc("/articles",articlesStoreHandler).Methods("POST").Name("articles.shore")
+
+	//自定义404页面
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	//通过命名路由获取 URL 事例
+	homeURL,_ := router.Get("home").URL()
+	fmt.Println(homeURL)
+	articleURL,_ := router.Get("articles.show").URL("id","23")
+	fmt.Println(articleURL)
+
 
 	http.ListenAndServe(":3000", router)
 }
