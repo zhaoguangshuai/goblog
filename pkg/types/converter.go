25c58dcd18efbb888555e96d3c53b190aa83c939
赵光帅 <969426998zgs@gmail.com>
2021-02-04 16:49:09 +0800

    新建 article模型

--- pkg/types/converter.go
+++ pkg/types/converter.go
@@ -1,8 +1,19 @@
 package types
 
-import "strconv"
+import (
+	"goblog/pkg/logger"
+	"strconv"
+)
 
 // Int64ToString 将 int64 转换为 string
 func Int64ToString(num int64) string {
 	return strconv.FormatInt(num, 10)
+}
+
+func StringToInt(str string) int {
+	i,err := strconv.Atoi(str)
+	if err != nil {
+		logger.LogError(err)
+	}
+	return i
 }
