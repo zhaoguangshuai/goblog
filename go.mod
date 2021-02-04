f4f4abd62a7aaef0a45af653657d9f261ae152af
赵光帅 <969426998zgs@gmail.com>
2021-02-04 09:18:29 +0800

    完成第一个测试

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.8.0
+	github.com/stretchr/testify v1.7.0
 )
---
+++ tests/pages_test.go
@@ -0,0 +1,22 @@
+package tests
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestHomePage(t *testing.T) {
+	baseURL := "http://localhost:3000"
+	//1.请求--模拟用户访问浏览器
+	var (
+		resp *http.Response
+		err  error
+	)
+	resp, err = http.Get(baseURL + "/")
+
+	//2. 检测--是否无错误且 200
+	assert.NoError(t,err,"有错误发生，err 不为空")
+	assert.Equal(t,200,resp.StatusCode,"应返回状态码 200")
+
+}
