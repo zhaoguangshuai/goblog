136508b4244d763458eb197e033482d5b7761603
赵光帅 <969426998zgs@gmail.com>
2021-02-05 11:05:42 +0800

    切割模版

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -9,6 +9,7 @@ import (
 	"gorm.io/gorm"
 	"html/template"
 	"net/http"
+	"path/filepath"
 	"strconv"
 	"unicode/utf8"
 )
@@ -63,11 +64,29 @@ func (*ArticlesController) Index(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "500 服务器内部错误")
 	} else {
 		//2.加载模版
-		tmpl, err := template.ParseFiles("resources/views/articles/index.gohtml")
+		//tmpl, err := template.ParseFiles("resources/views/articles/index.gohtml")
+		//logger.LogError(err)
+		//
+		////3.渲染模版，将所有文章的数据传输进去
+		//tmpl.Execute(w, articles)
+
+		//2.0 设置模版相对路径
+		viewDir := "resources/views"
+
+		//2.1 所有布局模版文件 slice
+		files,err := filepath.Glob(viewDir + "/layouts/*.gohtml")
 		logger.LogError(err)
 
-		//3.渲染模版，将所有文章的数据传输进去
-		tmpl.Execute(w, articles)
+		//2.2 在slice 里新增我们的目标文件
+		newFiles := append(files,viewDir+"/articles/index.gohtml")
+
+		//2.3 解析模版文件
+		tmpl,err := template.ParseFiles(newFiles...)
+		logger.LogError(err)
+
+		//2.4 渲染模版，将所有文章的数据传输进去
+		tmpl.ExecuteTemplate(w,"app",articles)
+
 	}
 
 }
