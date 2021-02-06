2db2900524c287fec622cd2b19f24995bdf8ffd5
赵光帅 <969426998zgs@gmail.com>
2021-02-06 20:11:24 +0800

    创建用户

---
+++ app/models/user/crud.go
@@ -0,0 +1,15 @@
+package user
+
+import (
+	"goblog/pkg/logger"
+	"goblog/pkg/model"
+)
+
+//Create 创建用户，通过 User.ID来判断是否创建成功
+func (user *User) Create() (err error) {
+	if err = model.DB.Create(&user).Error;err != nil {
+		logger.LogError(err)
+		return err
+	}
+	return nil
+}
--- bootstrap/db.go
+++ bootstrap/db.go
@@ -1,7 +1,10 @@
 package bootstrap
 
 import (
+	"goblog/app/models/article"
+	"goblog/app/models/user"
 	"goblog/pkg/model"
+	"gorm.io/gorm"
 	"time"
 )
 
@@ -19,4 +22,17 @@ func SetupDB()  {
 	sqlDB.SetMaxIdleConns(25)
 	// 设置每个链接的过期时间
 	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+
+	//创建和维护数据表结构
+	migration(db)
 }
+
+func migration(db *gorm.DB)  {
+	//自动迁移
+	db.AutoMigrate(
+		&user.User{},
+		&article.Article{},
+	)
+}
+
+
