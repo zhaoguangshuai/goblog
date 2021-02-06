bb533c98ca7acc96024557cf47e70cec70cc4619
赵光帅 <969426998zgs@gmail.com>
2021-02-06 21:10:00 +0800

    注册表单验证

--- app/models/user/user.go
+++ app/models/user/user.go
@@ -6,7 +6,10 @@ import "goblog/app/models"
 type User struct {
 	models.BaseModel
 
-	Name		string`gorm:"column:name;type:varchar(255);not null;unique"`
-	Email		string `gorm:"column:email;type:varchar(255);default:NULL;unique"`
-	Password	string `gorm:"column:password;type:varchar(255)"`
+	Name		string`gorm:"type:varchar(255);not null;unique" valid:"name"`
+	Email		string `gorm:"type:varchar(255);unique" valid:"email"`
+	Password	string `gorm:"type:varchar(255)" valid:"password"`
+
+	//gorm:"-" -- 设置 gorm 在读写诗略过此字段，仅用于表单验证
+	PasswordConfirm string `gorm:"-" valid:"password_confirm"`
 }
