670932a84feb04a2452832ea2629887022a98dca
赵光帅 <969426998zgs@gmail.com>
2021-02-07 16:01:59 +0800

    flash 消息提示

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -5,6 +5,7 @@ import (
 	"goblog/app/models/user"
 	"goblog/app/requests"
 	"goblog/pkg/auth"
+	"goblog/pkg/flash"
 	"goblog/pkg/view"
 	"net/http"
 )
@@ -50,6 +51,7 @@ func (*AuthController) DoRegister(w http.ResponseWriter,r *http.Request)  {
 		_user.Create()
 
 		if _user.ID > 0 {
+			flash.Success("恭喜您注册成功")
 			auth.Login(_user)
 			http.Redirect(w,r,"/",http.StatusFound)
 		} else {
@@ -74,6 +76,7 @@ func (*AuthController) DoLogin(w http.ResponseWriter,r *http.Request)  {
 	//2. 尝试登陆
 	if err := auth.Attempt(email,password);err == nil {
 		//登陆成功
+		flash.Success("欢迎回来！")
 		http.Redirect(w,r,"/",http.StatusFound)
 	} else {
 		//3.失败，显示错误提示
@@ -88,6 +91,7 @@ func (*AuthController) DoLogin(w http.ResponseWriter,r *http.Request)  {
 //Logout 退出登录
 func (*AuthController) Logout(w http.ResponseWriter,r *http.Request)  {
 	auth.Logout()
+	flash.Success("您已退出登陆")
 	http.Redirect(w,r,"/",http.StatusFound)
 }
 
